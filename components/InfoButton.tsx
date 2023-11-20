951dfd39ef4f123b746a748d4bd014772f3cdd31
JWittmeyer <jens.wittmeyer@kern.ai>
2023-11-20 18:30:25 +0100

    Adds button color class & further config for infobutton

--- components/InfoButton.tsx
+++ components/InfoButton.tsx
@@ -1,18 +1,20 @@
 import { combineClassNames } from "@/submodules/javascript-functions/general";
 import { useDefaults } from "@/submodules/react-components/hooks/useDefaults";
 import { IconInfoCircle } from "@tabler/icons-react";
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { Dispatch, Fragment, SetStateAction, useEffect, useRef, useState } from "react";
 import BaseModal from "../../../src/components/Common/Modal";
 import { InformationCircleIcon } from "@heroicons/react/20/solid";
 import useOnClickOutside from "@/submodules/react-components/hooks/useHooks/useOnClickOutside";
-import { useConsoleLog } from "@/submodules/react-components/hooks/useConsoleLog";
+import { Transition } from "@headlessui/react";
 
 type InfoButtonProps = {
     content: string | JSX.Element;
-    size?: "xs" | "sm" | "md" | "lg";
+    infoButtonSize?: "xs" | "sm" | "md" | "lg";
+    infoButtonColorClass?: string;
     access?: "hover" | "click";
     display?: "absoluteDiv" | "modal"
     divPosition?: "top" | "bottom" | "left" | "right"; // only for absoluteDiv relevant
+    divZIndexClass?: string;
 }
 
 type InfoConfig = {
@@ -26,19 +28,11 @@ type InfoConfig = {
 function generateAndCheckConfig(props: InfoButtonProps, setOpen: Dispatch<SetStateAction<boolean>>): InfoConfig {
     if (props.access == "hover" && props.display == "modal") console.warn("InfoButton - Hover access and modal display are not recommended")
     const config: any = {};
-    switch (props.size) {
-        case "xs":
-            config.size = 16;
-            break;
-        case "sm":
-            config.size = 24;
-            break;
-        case "md":
-            config.size = 48;
-            break;
-        case "lg":
-            config.size = 96;
-            break;
+    switch (props.infoButtonSize) {
+        case "xs": config.size = 16; break;
+        case "sm": config.size = 24; break;
+        case "md": config.size = 48; break;
+        case "lg": config.size = 96; break;
         default:
             console.error("InfoButton - Invalid size")
     }
@@ -63,7 +57,8 @@ const DEFAULT_VALUES = {
     size: "sm",
     access: "hover",
     display: "absoluteDiv",
-    divPosition: "top"
+    divPosition: "top",
+    divZIndexClass: "z-10"
 }
 
 export function InfoButton(_props: InfoButtonProps) {
@@ -71,41 +66,51 @@ export function InfoButton(_props: InfoButtonProps) {
     const [config, setConfig] = useState<InfoConfig>(null);
     const [open, setOpen] = useState(false);
 
-    useEffect(() => setConfig(generateAndCheckConfig(props, setOpen)), [props.size, props.access, props.display]);
+    useEffect(() => setConfig(generateAndCheckConfig(props, setOpen)), [props.infoButtonSize, props.access, props.display, props.divPosition]);
 
     if (!config) return null;
 
-
     return <>
         <div className={combineClassNames("relative w-fit p-1", config.cursorClass)} onClick={props.access == 'click' ? config.showInfo : undefined} onMouseEnter={props.access == 'hover' ? config.showInfo : undefined} onMouseLeave={props.access == 'hover' ? config.hideInfo : undefined}>
+            <IconInfoCircle size={config.size} className={props.infoButtonColorClass} />
             {props.display == "absoluteDiv" ? <RenderDiv
-                size={config.size}
                 positionClass={config.positionClass}
                 content={props.content}
                 open={open}
                 access={props.access}
                 onMouseEnter={config.showInfo}
-                onMouseLeave={config.hideInfo} /> : <IconInfoCircle size={config.size} />}
+                onMouseLeave={config.hideInfo}
+                zIndexClass={props.divZIndexClass} /> : null}
         </div>
         {props.display == "modal" ? <RenderModal content={props.content} open={open} setOpen={setOpen} /> : null}
     </>
 }
 
-function RenderDiv({ size, positionClass, open, content, access, onMouseEnter, onMouseLeave }: { size: number, positionClass: string, open: boolean, content: string | JSX.Element, access: string, onMouseEnter: () => void, onMouseLeave: () => void }) {
+function RenderDiv({ positionClass, open, content, access, onMouseEnter, onMouseLeave, zIndexClass }: { positionClass: string, open: boolean, content: string | JSX.Element, access: string, onMouseEnter: () => void, onMouseLeave: () => void, zIndexClass: string }) {
     const ref = useRef(null);
     useOnClickOutside(ref, onMouseLeave);
     return (<>
-        <IconInfoCircle size={size} />
-        <div ref={ref} className={combineClassNames("absolute bg-blue-50 p-2 border border-blue-400 cursor-auto z-50", positionClass, open ? '' : 'hidden')} onMouseEnter={access == 'hover' ? onMouseEnter : undefined} onMouseLeave={access == 'hover' ? onMouseLeave : undefined} >
-            {typeof content == "string" ?
-                <div className="flex items-center gap-x-2">
-                    <div className="flex-shrink-0">
-                        <InformationCircleIcon className="h-5 w-5 text-blue-400" aria-hidden="true" />
-                    </div>
-                    <p className="text-sm text-blue-700 w-max max-w-sm">{content}</p>
-                </div> : content}
-        </div>
-
+        <Transition.Root show={open} as={Fragment} >
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity ease-out duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div ref={ref} className={combineClassNames("absolute bg-blue-50 p-2 border border-blue-400 cursor-auto", positionClass, zIndexClass)} onMouseEnter={access == 'hover' ? onMouseEnter : undefined} onMouseLeave={access == 'hover' ? onMouseLeave : undefined} >
+                    {typeof content == "string" ?
+                        <div className="flex items-center gap-x-2">
+                            <div className="flex-shrink-0">
+                                <InformationCircleIcon className="h-5 w-5 text-blue-400" aria-hidden="true" />
+                            </div>
+                            <p className="text-sm text-blue-700 w-max max-w-sm">{content}</p>
+                        </div> : content}
+                </div>
+            </Transition.Child>
+        </Transition.Root>
     </>
     )
 }
