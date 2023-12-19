649db93833e9c7442cd16e1a14794913df393b22
Felix Kirsch <125893779+FelixKirschKern@users.noreply.github.com>
2023-12-19 11:03:18 +0100

    table loader (#8)
    Co-authored-by: Lennart Schmidt <lennart.schmidt@kern.ai>

--- components/NoTableEntriesYet.tsx
+++ components/NoTableEntriesYet.tsx
@@ -1,7 +1,7 @@
 import { memo } from "react";
 import { useDefaults } from "../hooks/useDefaults";
 import { NO_TABLE_ENTRIES_YET_DEFAULTS, NoTableEntriesYetProps } from "../types/noTableEntriesYet";
-
+import { Loading } from '@nextui-org/react';
 
 function GetNoTableEntriesYet(_props: NoTableEntriesYetProps) {
     const [props] = useDefaults<NoTableEntriesYetProps>(_props, NO_TABLE_ENTRIES_YET_DEFAULTS);
@@ -9,7 +9,12 @@ function GetNoTableEntriesYet(_props: NoTableEntriesYetProps) {
     return <tr>
         <td colSpan={props.tableColumns}>
             <div className={`flex justify-center items-center w-full text-sm ${props.heightClass + " " + props.backgroundColorClass + " " + props.textColorClass + " " + props.marginBottomClass}`}>
-                {props.text}
+                {props.loading?
+                <div className={`flex ${props.loadingColorClass}`}>
+                    <Loading size="md" type="spinner" color="currentColor"/>
+                </div>
+                :
+                props.text}
             </div>
         </td>
     </tr>
