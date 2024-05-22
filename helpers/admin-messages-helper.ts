423706de6b15e6e8122fb282548860d95edfb360
Lina <lina.lumburovska@kern.ai>
2024-05-22 12:04:22 +0200

    Admin messages as part of the submodules

---
+++ helpers/admin-messages-helper.ts
@@ -0,0 +1,20 @@
+import { AdminMessage, AdminMessageLevel } from "@/submodules/react-components/types/admin-messages";
+import { parseUTC } from "@/submodules/javascript-functions/date-parser";
+
+export const adminMessageLevels = {
+    [AdminMessageLevel.INFO]: { label: 'Info', color: 'blue' },
+    [AdminMessageLevel.WARNING]: { label: 'Warning', color: 'yellow' }
+};
+
+export function postProcessAdminMessages(adminMessages: AdminMessage[]): AdminMessage[] {
+    return adminMessages.map((message) => {
+        message = { ...message };
+        message.displayDate = parseUTC(message.archiveDate);
+        const color = adminMessageLevels[message.level].color;
+        message.textColor = 'text-' + color + '-700';
+        message.backgroundColor = 'bg-' + color + '-100';
+        message.borderColor = 'border-' + color + '-400';
+        message.visible = true;
+        return message;
+    });;
+}
