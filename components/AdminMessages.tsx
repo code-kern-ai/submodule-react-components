02bf29d0c13ef81176f0cf6586c7944ae5af0578
Lina <lina.lumburovska@kern.ai>
2024-05-29 09:35:53 +0200

    Added same font for admin messages

--- components/AdminMessages.tsx
+++ components/AdminMessages.tsx
@@ -13,7 +13,7 @@ export default function AdminMessages(props: AdminMessagesProps) {
         props.setActiveAdminMessages(adminMessagesCopy);
     }
 
-    return (<div className={`pointer-events-none flex-col right-0 fixed inset-y-0 sm:flex sm:justify-end sm:px-6 sm:pb-5 lg:px-8 ${isOnLabelingPage ? 'bottom-8' : 'bottom-0'}`}>
+    return (<div className={`pointer-events-none font-dm-sans flex-col right-0 fixed inset-y-0 sm:flex sm:justify-end sm:px-6 sm:pb-5 lg:px-8 ${isOnLabelingPage ? 'bottom-8' : 'bottom-0'}`}>
         {props.adminMessages && props.adminMessages.map((activeMessage, index) => (
             <div key={activeMessage.id} className={`pointer-events-auto items-center justify-between gap-x-6 py-2.5 mt-2 border px-6 sm:rounded-xl sm:py-3 sm:pr-3.5 sm:pl-4 ${activeMessage.borderColor} ${activeMessage.backgroundColor} ${activeMessage.visible ? 'flex' : 'hidden'}`}
                 style={{ maxWidth: props.maxWidth ?? 'calc(100vw - 200px)' }}>
