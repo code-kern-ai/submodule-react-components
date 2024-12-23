13eb8b0503a9418395dd5c8c76d7e6241fee84e6
Lina <lina.lumburovska@kern.ai>
2024-12-23 11:17:09 +0100

    Props to input with clear button

--- components/InputWithClearIcon.tsx
+++ components/InputWithClearIcon.tsx
@@ -3,11 +3,13 @@ import { IconX } from "@tabler/icons-react";
 export type InputWithClearIconProps = {
     value: string;
     onChange: (value: string) => void;
+    classNames?: string;
+    id?: string;
 }
 
 export default function InputWithClearIcon(props: InputWithClearIconProps) {
-    return (<div className="relative">
-        <input
+    return (<div className={`relative ${props.classNames}`}>
+        <input id={props.id} type="text" placeholder="Search"
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             value={props.value}
             onChange={(event) => props.onChange(event.target.value)}
