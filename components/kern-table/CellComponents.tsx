3bd1b4ec3d4e8b8ef64b76add0f635fa012a19eb
anmarhindi <anmar.hindi@kern.ai>
2025-02-06 13:08:51 +0100

    memoize classname

--- components/kern-table/CellComponents.tsx
+++ components/kern-table/CellComponents.tsx
@@ -265,14 +265,20 @@ function EvaluationRunStateCell({ value }) {
         }
     }, [value]);
 
-    return <span className={`inline-flex items-center px-2 py-0.5 rounded text-xs font-medium ${color === 'green'
-        ? 'bg-green-300'
-        : 'bg-' + color + '-100 text-' + color + '-800'} `}>
-        <svg className={`mr-1.5 h-2 w-2 ${'text-' + color + '-400'} `} fill="currentColor" viewBox="0 0 8 8">
-            <circle cx="4" cy="4" r="3" />
-        </svg>
-        <span className={' text-' + color + '-800'}>{value}</span>
-    </span>
+    const className = useMemo(() => {
+        return `inline-flex items-center px-2 py-0.5 rounded text-xs font-medium ${color === 'green'
+            ? 'bg-green-300'
+            : 'bg-' + color + '-100 text-' + color + '-800'}`;
+    }, [color]);
+
+    return (
+        <span className={className}>
+            <svg className={`mr-1.5 h-2 w-2 ${'text-' + color + '-400'}`} fill="currentColor" viewBox="0 0 8 8">
+                <circle cx="4" cy="4" r="3" />
+            </svg>
+            <span className={' text-' + color + '-800'}>{value}</span>
+        </span>
+    );
 }
 
 function EvaluationRunDetailsCell({ onClick, disabled }) {
