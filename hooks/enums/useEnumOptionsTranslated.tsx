fb91c6b67b1e8c531bf0b23a618eb8d9fe7224f9
JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
2025-08-14 11:46:27 +0200

    Conversation tagging (#53)
    * Additions to dropdown & svg icon
    
    * wip tag changes
    
    * Small extension
    
    * fixes typing on translated enum

--- hooks/enums/useEnumOptionsTranslated.tsx
+++ hooks/enums/useEnumOptionsTranslated.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 
 const cache = {};
 
-function getEnumOptionsForLanguage<T>(enumObj: T, lookupKey: string, t: any, language: string): { name: string, value: T }[] {
+function getEnumOptionsForLanguage<T>(enumObj: T, lookupKey: string, t: any, language: string): { name: string, value: T[keyof T] }[] {
     if (!(lookupKey in cache)) cache[lookupKey] = {};
     if (cache[lookupKey][language]) return cache[lookupKey][language];
     const finalArray = enumToArray(enumObj, { nameFunction: (s) => { return t(`${lookupKey}.${s}`) } });
@@ -16,7 +16,7 @@ function getEnumOptionsForLanguage<T>(enumObj: T, lookupKey: string, t: any, lan
 // unfortunately we need to provide the lookupKey ourselves which is the Enum name itself.
 // there is no way to get the name of the enum at runtime since they are compiled away
 // https://stackoverflow.com/a/59824941/19801189
-export default function useEnumOptionsTranslated<T>(enumObj: T, lookupKey: string, translationScope: string): { name: string, value: T }[] {
+export default function useEnumOptionsTranslated<T>(enumObj: T, lookupKey: string, translationScope: string): { name: string, value: T[keyof T] }[] {
     const { t, i18n } = useTranslation(translationScope);
     const values = useMemo(() => getEnumOptionsForLanguage(enumObj, lookupKey, t, i18n.language), [i18n.language]);
     return values;
