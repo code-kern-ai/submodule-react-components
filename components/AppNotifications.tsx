3983d43539540ff0c504a5bf555710218e1f927f
Lina <lina.lumburovska@kern.ai>
2025-10-02 14:36:58 +0200

    Notifications from admin dashboard

--- components/AppNotifications.tsx
+++ components/AppNotifications.tsx
@@ -10,40 +10,39 @@ import { UserRole } from "@/submodules/javascript-functions/enums/enums";
 
 type AppNotificationsProps = {
     project: { customerColorPrimary: string; }
-    user: { role: UserRole; }
+    user: { role: UserRole; languageDisplay: string; };
     forChatArea?: boolean;
+    notifications: any[];
 }
-// only last 3 are shown, id still needs to be counted up since the local storage is used to check if the notification was already shown
-// key is used for i18n translation
-const NOTIFICATIONS = [
-    { id: 1, key: "release_1_8", link: "https://www.kern.ai/resources/product-updates/week-28" },
-    { id: 2, key: "release_1_9", link: "https://www.kern.ai/resources/product-updates/week-37" }
-]
+
+const MIN_NOTIFICATIONS_SHOW = 3;
+const MAX_NOTIFICATIONS_SHOW = 7;
 
 export default function AppNotifications(props: AppNotificationsProps) {
-    const { t, i18n } = useTranslation('projectOverview');
+    const { t } = useTranslation('projectOverview');
     const [lastSeenNotification, setLastSeenNotification] = useLocalStorage<number>('lastSeen', 'releaseNotification', undefined, -1);
     const [showNotifications, setShowNotifications] = useState(false);
+    const [showMoreClicked, setShowMoreClicked] = useState(false);
     const justClickedOutsideRef = useRef(false); // to prevent showing the notifications if the user just clicked outside
     const refNotificationBox = useRef(null);
     useOnClickOutside(refNotificationBox, () => { setShowNotifications(false); justClickedOutsideRef.current = true; setTimeout(() => justClickedOutsideRef.current = false, 200); });
+
     const clickBell = useCallback(() => {
         if (justClickedOutsideRef.current) return; // if the user just clicked outside, don't show the notifications since the person is trying to close via bell icon
         setShowNotifications(true);
-        setLastSeenNotification(NOTIFICATIONS[NOTIFICATIONS.length - 1].id);
-    }, []);
+        setLastSeenNotification(props.notifications[props.notifications.length - 1].id);
+    }, [props.notifications]);
 
-    const finalNotifications = useMemo(() => NOTIFICATIONS.slice(-3).map((e) => (
-        {
-            ...e,
-            headline: t("notificationBell." + e.key + ".headline", props.user?.role == UserRole.ENGINEER ? { lng: "en" } : undefined),
-            description: t("notificationBell." + e.key + ".description", props.user?.role == UserRole.ENGINEER ? { lng: "en" } : undefined)
-        })), [NOTIFICATIONS, i18n.language, props.user?.role]);
+    const finalNotifications = useMemo(() => {
+        if (props.notifications.length <= MIN_NOTIFICATIONS_SHOW) return props.notifications;
+        if (!showMoreClicked) return props.notifications.slice(-MIN_NOTIFICATIONS_SHOW);
+        if (showMoreClicked) return props.notifications.slice(-MAX_NOTIFICATIONS_SHOW);
+    }, [props.notifications, showMoreClicked]);
 
     const hasNewNotifications = useMemo(() => {
         if (lastSeenNotification === -1) return true; // if no notification was seen
-        return finalNotifications.some(notification => notification.id > lastSeenNotification);
-    }, [lastSeenNotification, finalNotifications]);
+        return props.notifications.some(notification => notification.id > lastSeenNotification);
+    }, [lastSeenNotification, props.notifications]);
 
     const isLightDesign = useMemo(() => tinycolor(props.project?.customerColorPrimary).isLight(), [props.project?.customerColorPrimary]);
 
@@ -61,21 +60,24 @@ export default function AppNotifications(props: AppNotificationsProps) {
             <MemoIconBell />
         </button>
         {hasNewNotifications && <div className={combineClassNames("absolute w-2 h-2 bg-red-500 rounded-full pointer-events-none", props.forChatArea ? 'top-1 right-1' : 'top-2 right-2')}></div>}
-        {showNotifications && <div className={combineClassNames("absolute overflow-hidden -translate-y-2 bottom-full left-0 w-72 rounded-lg bg-slate-50 shadow-lg z-10", props.forChatArea ? '' : 'translate-x-2', props.forChatArea ? 'bg-[var(--background-color-menu)]' : '')} ref={refNotificationBox}>
+        {showNotifications && <div className={combineClassNames("absolute overflow-hidden -translate-y-2 bottom-full left-0 w-72 rounded-lg bg-slate-50 shadow-lg z-10 text-gray-700", props.forChatArea ? '' : 'translate-x-2', props.forChatArea ? 'bg-[var(--background-color-menu)]' : '')} ref={refNotificationBox}>
             <div className="flex flex-col">
-                <div className="bg-gray-800/25"><div className="uppercase py-2 px-3">{t("notificationBell.header", props.user?.role == UserRole.ENGINEER ? { lng: "en" } : undefined)}</div></div>
+                <div className="bg-gray-800/25 text-gray-900"><div className="uppercase py-2 px-3">{t("notificationBell.header")}</div></div>
                 {finalNotifications.map((notification, idx) => (
                     <div key={notification.id} className={combineClassNames("py-4 px-3", idx == finalNotifications.length - 1 ? '' : 'border-b border-slate-400')}>
-                        <div className="font-semibold">{notification.headline}</div>
-                        <div className="text-xs line-clamp-3">{notification.description}</div>
+                        <div className="font-semibold">{notification.config[props.user?.languageDisplay].headline}</div>
+                        <div className="text-xs line-clamp-3">{notification.config[props.user?.languageDisplay].description}</div>
                         <Link
                             href={notification.link}
                             className={'text-xs mt-1' + (isLightDesign ? ' text-red-800' : ' text-red-600')}
                             target="_blank"
-                        >{t("notificationBell.link", props.user?.role == UserRole.ENGINEER ? { lng: "en" } : undefined)}</Link>
+                        >{t("notificationBell.link")}</Link>
                     </div>
                 ))}
             </div>
+            {props.notifications.length > MIN_NOTIFICATIONS_SHOW && <div className="py-1 px-3 bg-gray-800/25 text-center">
+                <label className="text-xs text-gray-500 cursor-pointer" onClick={() => setShowMoreClicked(!showMoreClicked)}>{!showMoreClicked ? t("notificationBell.showMore") : t("notificationBell.showLess")}</label>
+            </div>}
         </div>
         }
     </div>
