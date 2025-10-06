cf07fa48470b785466b5280e5675ba7df3be56fd
Lina <lina.lumburovska@kern.ai>
2025-10-06 16:20:26 +0200

    Small UI text changes

--- components/AppNotifications.tsx
+++ components/AppNotifications.tsx
@@ -78,7 +78,7 @@ export default function AppNotifications(props: AppNotificationsProps) {
                 {finalNotifications.length === 0 && <div className="py-4 px-3 text-sm text-gray-500">{t("notificationBell.noNotifications")}</div>}
             </div>
             {props.notifications.length > MIN_NOTIFICATIONS_SHOW && <div className="py-1 px-3 bg-gray-800/25 text-center">
-                <label className="text-xs text-gray-500 cursor-pointer" onClick={() => setShowMoreClicked(!showMoreClicked)}>{!showMoreClicked ? t("notificationBell.showMore") : t("notificationBell.showLess")}</label>
+                <label className="text-xs text-gray-900 cursor-pointer" onClick={() => setShowMoreClicked(!showMoreClicked)}>{!showMoreClicked ? t("notificationBell.showMore") : t("notificationBell.showLess")}</label>
             </div>}
         </div>
         }
