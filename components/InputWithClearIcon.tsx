ce97cdc6610e5eea7738aed25c732bb9120d6174
Lina <lina.lumburovska@kern.ai>
2026-04-27 09:55:12 +0200

    Placeholder added to InputWithClearIcon

--- components/InputWithClearIcon.tsx
+++ components/InputWithClearIcon.tsx
@@ -7,6 +7,7 @@ export type InputWithClearIconProps = {
     onChange: (value: string) => void;
     classNames?: string;
     id?: string;
+    placeholder?: string;
 }
 
 export default function InputWithClearIcon(props: InputWithClearIconProps) {
@@ -16,7 +17,7 @@ export default function InputWithClearIcon(props: InputWithClearIconProps) {
     }, [props.onChange]);
 
     return (<div className={`relative ${props.classNames}`}>
-        <input id={props.id} type="text" placeholder="Search"
+        <input id={props.id} type="text" placeholder={props.placeholder ?? "Search"}
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             value={props.value}
             onChange={(event) => props.onChange(event.target.value)}
